54e9d338550526acbca073cf3033b7258a17021a
River Zhou <riverzhou2000@163.com>
2020-12-01 23:27:56 +0800

    update policys

--- pai/mine/1504/fd_control/policy_J.py
+++ pai/mine/1504/fd_control/policy_J.py
@@ -4,8 +4,8 @@
 
 image_trigger   = [('10:34:00', 75200), ('11:29:35', 300), ('11:29:50', 300)]
 
-decode_type     = ['A', 'B', 'C']
-decode_timeout  = [30, 15, 2]
+decode_type     = ['A', 'C', 'C']
+decode_timeout  = [30, 8, 2]
 image_timeout   = [30, 7, 7]
 price_timeout   = [30, 2, 10]
 
